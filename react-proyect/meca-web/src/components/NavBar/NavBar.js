16bf01e4ca2c9754c9a9721a521c830a6f3c47d2
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2022-05-18 17:47:00 -0300

    Animación y movimientos de NavBar

--- react-proyect/meca-web/src/components/NavBar/NavBar.js
+++ react-proyect/meca-web/src/components/NavBar/NavBar.js
@@ -1,17 +1,42 @@
 import './NavBar.css';
 import {Link} from 'react-router-dom';
 import logoMecaIsologo from '../../imagenes/meca-logo-isologo.png';
+import logoMecaTexto from '../../imagenes/logo-meca-text.png';
 
 function NavBar() {
+
+    const HideNavBar = () => {
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+            if (200 > currentScrollPos) {
+                document.getElementById('navBar').style.background = '';
+                document.getElementById('navBar').style.height = '150px';
+                document.getElementById('navBar').style.paddingBottom = '0';
+                document.getElementById('navBar').style.borderRadius = '0'
+                document.getElementById('logo-navBar').src = logoMecaIsologo;
+                document.getElementById('logo-navBar').style.marginBottom = '0';
+            } else {
+                document.getElementById('navBar').style.background = '#ffffffea ';
+                document.getElementById('navBar').style.height = '80px';
+                document.getElementById('navBar').style.paddingBottom = '20px';
+                document.getElementById('navBar').style.borderBottomLeftRadius = '30px';
+                document.getElementById('navBar').style.borderBottomRightRadius = '30px';
+                document.getElementById('logo-navBar').src = logoMecaTexto;
+                document.getElementById('logo-navBar').style.marginBottom = '-35px';
+            }
+        }
+    }
+
+    HideNavBar();
 
     return (
         <div className="navbar">
             <div className="background"></div>
             <div className="container-navigation">
-                <div className="navigation">
-                    <div className='container-logo-marca'>
+                <div className="navigation" id='navBar'>
+                    <div className='container-logo-marca' id='logo-nav-bar'>
                         <Link to = {'/'}>
-                            <img className='logo-pagina' alt='Logo' src={logoMecaIsologo}></img>
+                            <img id='logo-navBar' className='logo-pagina' alt='Logo' src={logoMecaIsologo}></img>
                         </Link>
                     </div>
                     <ul className='navbar-style'>
@@ -42,6 +67,7 @@ function NavBar() {
                         </li>
                     </ul>
                 </div>
+                <div className="bottom-gradiente"></div>
             </div>
         </div>
     );
--- react-proyect/meca-web/src/components/SectionContactanos/SectionContactanos.js
+++ react-proyect/meca-web/src/components/SectionContactanos/SectionContactanos.js
@@ -8,7 +8,9 @@ function SectionContactanos() {
             <h4>Una experiencia de calidad</h4>
             <p>Diseño, fabricación y puesta en marcha de tableros eléctricos para la generación y distribución de energía eléctrica.</p>
             <Link to = {'/contactanos'}>
-                <div className="buttom-contactanos"><h3>Contáctanos</h3></div>
+            <div className="buttom-contactanos">
+                <h3>Contáctanos</h3>
+            </div>
             </Link>
         </div>
     );
