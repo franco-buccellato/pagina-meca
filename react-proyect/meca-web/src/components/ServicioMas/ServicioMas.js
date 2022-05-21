c326bd3d644d778a5132978e1cf7b4215a2ee370
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2022-05-20 23:01:44 -0300

    Detalles CSS de Servicios

--- react-proyect/meca-web/src/components/ServicioMas/ServicioMas.js
+++ react-proyect/meca-web/src/components/ServicioMas/ServicioMas.js
@@ -1,7 +1,6 @@
 import './ServicioMas.css';
 import servicioMas from '../../imagenes/mas-servicios.png';
 
-
 /*SVG Mas*/
 import linea16Mas from '../../imagenes/lineas-mas/Line 16despliegue-mas-servicios.svg';
 import linea17Mas from '../../imagenes/lineas-mas/Line 17despliegue-mas-servicios.svg';
@@ -14,20 +13,20 @@ function ServicioMas({onAdd}) {
 
     return (
         <div className="section-servicios">
-        <h3 className='section-servicos-tittle'>Más Servicios</h3>
-        <div className="container-section-servicios-mas">
-            <div className="servicios" onClick={() => onAdd()}>
-                <img className='servicio-mas' alt='Logo Servicio Mas' src={servicioMas}></img>
-            </div>
-            <div className="container-section-servicios-mas-lineas">
-                <div className="container-section-servicios-mas-linea-1"><img alt='Linea 16 mas' src={linea16Mas}></img><span>Tableros hospitalarios.</span></div>
-                <div className="container-section-servicios-mas-linea-2"><img alt='Linea 17 mas' src={linea17Mas}></img><span>Tableros de transferencia automática.</span></div>
-                <div className="container-section-servicios-mas-linea-3"><img alt='Linea 21 mas' src={linea20Mas}></img><span>Tableros de automatismos para sistemas de generación</span></div>
-                <div className="container-section-servicios-mas-linea-4"><img alt='Linea 22 mas' src={linea21Mas}></img><span>Tableros de monitoreo de parámetros eléctricos.</span></div>
-                <div className="container-section-servicios-mas-linea-5"><img alt='Linea 23 mas' src={linea22Mas}></img><span>Tableros para BMS y domótica.</span></div>
+            <h3 className='section-servicos-tittle'>Más Servicios</h3>
+            <div className="container-section-servicios-mas">
+                <div className="container-section-servicios-mas-lineas">
+                    <div className="container-section-servicios-mas-linea-1"><span>Medición y mantenimiento de sistemas de puesta a tierra.</span><img alt='Linea 21 mas' src={linea21Mas}></img></div>
+                    <div className="container-section-servicios-mas-linea-2"><span>Instalación de sistemas de puesta a tierra.</span><img alt='Linea 17 mas' src={linea17Mas}></img></div>
+                    <div className="container-section-servicios-mas-linea-3"><span>Tendido de conductores de media y baja tensión con sus canalizaciones.</span><img alt='Linea 22 mas' src={linea22Mas}></img></div>
+                    <div className="container-section-servicios-mas-linea-4"><span>Instalaciones eléctricas en Datacenters.</span><img alt='Linea 16 mas' src={linea16Mas}></img></div>
+                    <div className="container-section-servicios-mas-linea-5"><span>Retrofit / Ecofit de interruptores.</span><img alt='Linea 20 mas' src={linea20Mas}></img></div>
+                </div>
+                <div className="servicios" onClick={() => onAdd()}>
+                    <img className='servicio-mas' alt='Logo Servicio Mas' src={servicioMas}></img>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
--- react-proyect/meca-web/src/components/ServicioMontaje/ServicioMontaje.js
+++ react-proyect/meca-web/src/components/ServicioMontaje/ServicioMontaje.js
@@ -12,14 +12,16 @@ function ServicioMontaje({onAdd}) {
         <div className="section-servicios">
             <h3 className='section-servicos-tittle'>Montaje e Instalación</h3>
             <div className="container-section-servicios-montaje">
+                <div className="container-section-servicios-montaje-lineas">
+                    <div className="container-section-servicios-montaje-linea-1"><span>De tableros en baja tensión.</span><img alt='Linea 22 montaje' src={linea22Montaje}></img></div>
+                    <div className="container-section-servicios-montaje-linea-2"><span>De celdas de media tensión.</span><img alt='Linea 23 montaje' src={linea23Montaje}></img></div>
+                </div>
                 <div className="servicios" onClick={() => onAdd()}>
                     <img className='servicio-montaje' alt='Logo Servicio Montaje' src={serviciomontaje}></img>
                 </div>
                 <div className="container-section-servicios-montaje-lineas">
-                    <div className="container-section-servicios-montaje-linea-1"><img alt='Linea 20 montaje' src={linea20Montaje}></img><span>Tableros petroleros.</span></div>
-                    <div className="container-section-servicios-montaje-linea-2"><img alt='Linea 21 montaje' src={linea21Montaje}></img><span>Tableros de automatismos para sistemas de generación</span></div>
-                    <div className="container-section-servicios-montaje-linea-3"><img alt='Linea 22 montaje' src={linea22Montaje}></img><span>Tableros de monitoreo de parámetros eléctricos.</span></div>
-                    <div className="container-section-servicios-montaje-linea-4"><img alt='Linea 23 montaje' src={linea23Montaje}></img><span>Tableros para BMS y domótica.</span></div>
+                    <div className="container-section-servicios-montaje-linea-3"><img alt='Linea 20 montaje' src={linea20Montaje}></img><span>De grupos electrógenos.</span></div>
+                    <div className="container-section-servicios-montaje-linea-4"><img alt='Linea 21 montaje' src={linea21Montaje}></img><span>De UPS's estáticas.</span></div>
                 </div>
             </div>
         </div>
