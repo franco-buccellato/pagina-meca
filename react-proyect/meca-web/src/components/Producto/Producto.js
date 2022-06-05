2a86970d852c593ea0d6592e39df0f7ec772057f
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2022-06-05 18:28:27 -0300

    Imagenes de 1,2 y 3

--- react-proyect/meca-web/src/components/Producto/Producto.js
+++ react-proyect/meca-web/src/components/Producto/Producto.js
@@ -2,17 +2,16 @@ import './Producto.css';
 import { Link } from 'react-router-dom';
 const imagenProducto = require.context('../../imagenes/Productos/', true);
 
-const Producto = ({tableroId, link_imagenes, titulo, index}) => {
+const Producto = ({tableroId, titulo, cliente, obra, descripcion, link_imagenes, index}) => {
 
     return (
         <div className={'container-producto'+index}>
             <Link to ={`/productDetail/${tableroId}`}>
                 <div className="container-producto-background-1">
-                    {/* <img className='container-producto-imagen' alt='test' src={link_imagenes[0]}></img> */}
-                    <img
-                            alt={tableroId}
-                            src = {imagenProducto(`./` + link_imagenes[0])}
-                        ></img>
+{/*                     <img
+                        alt={tableroId}
+                        src = {imagenProducto(`./` + link_imagenes[0])}
+                    ></img> */}
                     <div className="container-producto-imagen-degrade"></div>
                     <div className="container-producto-background-2">
                         <h5>{titulo}</h5>
